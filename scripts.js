450b33a4cf0337c56f1e4f4907ba400c9f31103a
Jack Lingle <captainjack0818@gmail.com>
2023-03-06 12:43:49 -0500

    Add input to html and add flexboxes to css

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 const DEFAULTSIZE = 4;
 const SKETCHDISPLAY = document.getElementById('sketchDisplay');
+// loops through and creates grid
 function createGrid(gridSize = DEFAULTSIZE) {
     for(let i = 0; i < gridSize; i++) {
+        // create row
         let row = document.createElement('div');
         row.classList.add('row');
         for(let j = 0; j < gridSize; j++) {
+            // loop within row and add boxes across row
             let box = document.createElement('div');
             box.classList.add('box');
             row.appendChild(box);
@@ -12,4 +15,6 @@ function createGrid(gridSize = DEFAULTSIZE) {
         SKETCHDISPLAY.appendChild(row);
     }
 }
-createGrid();
+function getGridSize() {
+    
+}
